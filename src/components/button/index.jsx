583c7224ea3072c0551db47f0cfdce378b54f2c8
agkayacaner <dev.caneragkaya@gmail.com>
2023-10-06 20:55:31 +0300

    Added Headless Uı, Created Main Menu and components

---
+++ src/components/button/index.jsx
@@ -0,0 +1,28 @@
+import { createElement } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+export default function Button({ size, children }) {
+  return createElement(
+    'button',
+    {
+      className: classNames(
+        'bg-[#1d9bf0] rounded-full flex items-center justify-center font-bold hover:bg-[#1a8cd8] transition-colors',
+        {
+          'px-4 h-[52px] text-[17px] w-full': size === 'large',
+          'px-4 h-9': size === 'medium',
+          'px-3 h-8': size === 'small',
+        }
+      ),
+    },
+    children
+  );
+}
+
+Button.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+Button.defaultProps = {
+  size: 'medium',
+};
---
+++ src/layouts/main/sidebar/menu/newPost/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '~/components/button';
+
+export default function NewPost() {
+  return (
+    <div className='py-4 w-[90%]'>
+      <Button size='large'>Gönder</Button>
+    </div>
+  );
+}
